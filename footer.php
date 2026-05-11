0460eafc840225540d872a52ad9de328a51f03f5
marina <pi.mcm3.ko@gmail.com>
2026-05-11 12:54:03 +0900

    競合を解消

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     <footer>
         <p class="copy">&copy;2026ゲームチェンジ株式会社</p>
     </footer>
@@ -8,15 +7,4 @@
     <?php wp_footer(); ?>
 </body>
 
-=======
-    <footer>
-        <p class="copy">&copy;2026ゲームチェンジ株式会社</p>
-    </footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="js/main.js"></script>
-    <?php wp_footer(); ?>
-</body>
-
->>>>>>> ef0d8fcecf4ba6012413300936341911d88e3cb1
 </html>

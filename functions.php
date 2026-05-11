0782babd00f50a37bd44b1cab1e5b10e3e5d3998
marina <pi.mcm3.ko@gmail.com>
2026-05-11 12:40:52 +0900

    Initial commit

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+add_action('wp_enqueue_scripts', 'add_styles');
+
+function add_styles()
+{
+    // google fonts
+    wp_register_style(
+        'google-fonts_style',
+        'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap',
+        array(),
+        '1.0'
+    );
+
+    // reset style
+    wp_register_style(
+        'reset_style',
+        'https://unpkg.com/ress/dist/ress.min.css',
+        array(),
+        '1.0'
+    );
+
+    // main style
+    wp_enqueue_style(
+        'main_style',
+        get_template_directory_uri() . '/scss/style.css',
+        array('reset_style', 'google-fonts_style'),
+        '1.0'
+    );
+}
+?>
